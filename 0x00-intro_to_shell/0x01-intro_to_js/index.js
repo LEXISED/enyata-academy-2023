225687b7d32d4a03a9f6e61cb96fd850f5a77455
LEXISED <lexiseduah@gmail.com>
2023-05-25 15:35:46 -0700

    0x01-intro_to_js

---
+++ 0x00-intro_to_shell/0x01-intro_to_js/index.js
@@ -0,0 +1,32 @@
+let myName = "Nathan";
+
+
+console.log(typeof myName);
+
+
+myName = "Eduah-Lexis";
+
+let car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2023,
+    color: "Black",
+    engine: "3.5L",
+    transmission: "Automatic",
+    seats: 5,
+    fuelType: "Gasoline",
+    milage: 16000,
+    isElectric: false,
+};
+
+const favoriteNumber = 77;
+
+const halfofFavNum = favoriteNumber / 2;
+
+let isaacsFavorite = 13;
+
+let difference = isaacsFavorite - favoriteNumber;
+
+isaacsFavorite = isaacsFavorite * 26;
+
+isaacsFavorite = isaacsFavorite % 3;
