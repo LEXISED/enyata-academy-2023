7861d973d13203a3fcc749c7cc60197c7728c7d6
LEXISED <lexiseduah@gmail.com>
2023-05-27 18:46:11 -0700

    0x02-intro_to_js

--- 0x00-intro_to_shell/0x01-intro_to_js/index.js
+++ 0x00-intro_to_shell/0x01-intro_to_js/index.js
@@ -1,32 +1,49 @@
-let myName = "Nathan";
+//let myName = "Nathan";
 
 
-console.log(typeof myName);
+//console.log(typeof myName);
 
 
-myName = "Eduah-Lexis";
+//myName = "Eduah-Lexis";
 
-let car = {
-    make: "Toyota",
-    model: "Camry",
-    year: 2023,
-    color: "Black",
-    engine: "3.5L",
-    transmission: "Automatic",
-    seats: 5,
-    fuelType: "Gasoline",
-    milage: 16000,
-    isElectric: false,
-};
+ //let car = {
+   // make: "Toyota",
+    //model: "Camry",
+    //year: 2023,
+    //color: "Black",
+    //engine: "3.5L",
+    //transmission: "Automatic",
+    //seats: 5,
+    //fuelType: "Gasoline",
+    //milage: 16000,
+    //isElectric: false,
+//};
 
-const favoriteNumber = 77;
+//const favoriteNumber = 77;
 
-const halfofFavNum = favoriteNumber / 2;
+//const halfofFavNum = favoriteNumber / 2;
 
-let isaacsFavorite = 13;
+//let isaacsFavorite = 13;
 
-let difference = isaacsFavorite - favoriteNumber;
+//let difference = isaacsFavorite - favoriteNumber;
 
-isaacsFavorite = isaacsFavorite * 26;
+//isaacsFavorite = isaacsFavorite * 26;
 
-isaacsFavorite = isaacsFavorite % 3;
+//isaacsFavorite = isaacsFavorite % 3;
+
+
+// declare our function
+function convertKgToLb() {
+    let weightInKg = parseFloat(prompt("Enter the weight in kilograms:"));
+
+
+    if (isNaN(weightInKg)) {
+        alert("Invalid input. Please enter a number");
+        return;
+    }
+    let weightInLb = weightInKg *2.2;
+    alert(weightInKg + "kg =" + weightInLb + "lb");
+
+    return weightInLb;
+}
+convertKgToLb();
